1822c743513453b11ebcfd58bf0f03a9c585c790
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2025-06-27 13:10:15 +0530

    middleware done

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
 
 const isPublicRoute = createRouteMatcher([
@@ -16,6 +17,24 @@ const isPublicApiRoute = createRouteMatcher([
 export default clerkMiddleware(async (auth, req) => {
     const { userId } = await auth(); // await is needed or not.
     const currentUrl = new URL(req.url)
+    const isAccessingDashboard = currentUrl.pathname === '/home'
+    const isApiRequest = currentUrl.pathname.startsWith('/api')
+
+    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
+      return NextResponse.redirect(new URL('/home', req.url))
+    }
+    //not logged in
+    if (!userId){
+      // If the user is not logged in, redirect them to sign-in
+       if(!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL('/sign-in', req.url))
+    }
+       // If the user is not logged in and accessing an API route, redirect to sign-in
+    if(isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL('/signin', req.url))
+    }
+  }
+
 
 })
 
