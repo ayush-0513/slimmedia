f39554fc7a43de4993e0e5600bcd63ba29c2d10a
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2025-06-27 13:15:55 +0530

    middleware done

--- middleware.ts
+++ middleware.ts
@@ -34,15 +34,12 @@ export default clerkMiddleware(async (auth, req) => {
       return NextResponse.redirect(new URL('/signin', req.url))
     }
   }
-
+  return NextResponse.next()
+//without this u canot have moddleware
 
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/((?!.*\\..*|_next).*)","/","/(api|trpc)(.*)"],
+
+};
